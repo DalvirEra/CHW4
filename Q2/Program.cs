e015221cb1db27f29144e837fc0163b97434d2fb
Dalvir <ilya.hurs@gmail.com>
2022-12-09 22:20:50 +0300

    small changes

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -1,9 +1,14 @@
 ﻿Console.WriteLine("Введите 1 число ");
 int number = Convert.ToInt32(Console.ReadLine());
+int numberOrig = number;
 int sum = 0;
-while (number > 9){
-    sum += number%10;
+while (number > 10){
+    if ((numberOrig)%(number%10) == 0){
+        sum += number%10;
+    }
     number = number/10;
 }
-sum += number;
+if ((number%10)%number == 0){
+        sum += number%10;
+    }
 Console.WriteLine(sum);
