a2b2b047cf7d96ad0c316d9efac6156890ca8ebb
Dalvir <ilya.hurs@gmail.com>
2022-12-09 22:24:04 +0300

    small changes

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -2,13 +2,13 @@
 int number = Convert.ToInt32(Console.ReadLine());
 int numberOrig = number;
 int sum = 0;
-while (number > 10){
-    if ((numberOrig)%(number%10) == 0){
-        sum += number%10;
+while (number > 0){
+    if (number%10 != 0){
+        if ((numberOrig)%(number%10) == 0){
+            sum += number%10;
+        }
     }
     number = number/10;
 }
-if ((number%10)%number == 0){
-        sum += number%10;
-    }
+
 Console.WriteLine(sum);
